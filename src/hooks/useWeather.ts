c53b7deeb742d1ca52de4742ea06307f2e90c446
Andreza Sousa <andrezanascimentodesousa@gmail.com>
2025-06-02 16:07:11 -0300

    modifications added to the components, after adjustments to the API call

--- src/hooks/useWeather.ts
+++ src/hooks/useWeather.ts
@@ -1,3 +1,15 @@
+// // import { useQuery } from "@tanstack/react-query";
+// // import { api } from "../services/api";
+
+// // export const useWeather = (lat: number, lon: number) => {
+// //   return useQuery({
+// //     queryKey: ["weather", lat, lon],
+// //     queryFn: async () => {
+// //       const { data } = await api.get("", { params: { lat, lon } });
+// //       return data;
+// //     },
+// //   });
+// // };
 // import { useQuery } from "@tanstack/react-query";
 // import { api } from "../services/api";
 
@@ -5,9 +17,12 @@
 //   return useQuery({
 //     queryKey: ["weather", lat, lon],
 //     queryFn: async () => {
-//       const { data } = await api.get("", { params: { lat, lon } });
+//       const { data } = await api.get("", {
+//         params: { lat, lon, exclude: "minutely,hourly,alerts" },
+//       });
 //       return data;
 //     },
+//     staleTime: 1000 * 60 * 10, // 10 min
 //   });
 // };
 import { useQuery } from "@tanstack/react-query";
@@ -18,10 +33,18 @@ export const useWeather = (lat: number, lon: number) => {
     queryKey: ["weather", lat, lon],
     queryFn: async () => {
       const { data } = await api.get("", {
-        params: { lat, lon, exclude: "minutely,hourly,alerts" },
+        params: {
+          lat,
+          lon,
+          appid: import.meta.env.VITE_OPENWEATHER_API_KEY,
+          units: "metric",
+          lang: "pt_br",
+        },
       });
+      console.log("API Response:", data);
       return data;
     },
-    staleTime: 1000 * 60 * 10, // 10 min
+    enabled: lat !== 0 && lon !== 0,
+    staleTime: 1000 * 60 * 10,
   });
 };
