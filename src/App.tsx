c4e7c4b0bc0f97126e771d8c2634cbf87978b984
Andreza Sousa <andrezanascimentodesousa@gmail.com>
2025-06-04 17:30:14 -0300

    global style added

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+"use client";
+
+import { Provider } from "react-redux";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ThemeProvider } from "./context/ThemeContext";
+import WeatherApp from "./components/WeatherApp";
+import { store } from "./store/store";
+import "./global/global.css";
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <QueryClientProvider client={queryClient}>
+        <ThemeProvider>
+          <WeatherApp />
+        </ThemeProvider>
+      </QueryClientProvider>
+    </Provider>
   );
 }
 
